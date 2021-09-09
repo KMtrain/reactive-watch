642ba8860132daf2a9c9f060a35a7da491222ea2
KMtrain <gitTrainKM@gmail.com>
2021-09-09 13:24:17 +0300

    Watch via React + RxJS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Watch from './components/Watch';
 
-function App() {
+import { interval } from "rxjs";
+import { map } from "rxjs/operators";
+
+import "./App.css";
+
+
+
+
+const timeStep = 1000;
+
+export default function App(){
+
+  const [timer, setTimer] = useState(0);
+  const [diff, setDiff] = useState(0);
+
+  const [subscription, setSubscription] = useState("");
+  const [prevent, setPrevent] = useState(true);
+
+  const onStartHandler = () => {
+    if (!subscription) {
+      const timerSubscription = interval(timeStep)
+        .pipe(map((v) => v + 1))
+        .subscribe((v) => {
+          setTimer(v + diff);
+        });
+      setSubscription(timerSubscription);
+    } else {
+      subscription.unsubscribe();
+      setTimer(0);
+      setDiff(0);
+      setSubscription("");
+    }
+  };
+
+  const onWaitHandler = (event) => {
+    if (prevent) {
+      setPrevent(false);
+      const timerInstance = setTimeout(function () {
+        setPrevent(true);
+        clearTimeout(timerInstance);
+      }, 300);
+    } else {
+      if (subscription) {
+        subscription.unsubscribe();
+      }
+
+      setDiff(timer);
+      setSubscription("");
+    }
+  };
+
+  const onResetHandler = () => {
+    if (subscription) {
+      subscription.unsubscribe();
+    }
+
+    const timerSubscription = interval(timeStep).subscribe((v) => {
+      setTimer(v);
+    });
+    setSubscription(timerSubscription);
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Watch timeCurrentCount = {timer ? timer : diff} />
+
+      <div className="btn-container">
+        <button className="btn-containerStart" onClick={onStartHandler}>Start/Stop</button>
+        <button className="btn-containerWait" onClick={onWaitHandler}>Wait</button>
+        <button className="btn-containerReset" onClick={onResetHandler}>Reset</button>
+      </div>
+    
     </div>
-  );
+    
+  )
+  
+  
 }
 
-export default App;
--- src/Components/Watch.js
+++ src/Components/Watch.js
@@ -1,6 +1,33 @@
 import React from "react"
 import { useState, useEffect } from "react"
 
-import "WatchStyle.css"
+import "./WatchStyle.css"
+
+const Watch = (props) => {
+
+    const [hours, setHours] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+
+    useEffect(() => {
+
+        let hours = Math.floor(props.timeCurrentCount / 3600);
+        setHours(() => (hours.toString().length === 1 ? "0" : "") + hours);
+        
+        let minutes = Math.floor((props.timeCurrentCount % 3600) / 60);
+        setMinutes(() => (minutes.toString().length ===1 ? "0" : "") + minutes);
+
+        let seconds = props.timeCurrentCount % 60;
+        setSeconds(() => (seconds.toString().length ===1 ? "0" : "") + seconds);
+
+    }, [props.timeCurrentCount]);
 
 
+    return (
+        <div>
+            <h1 className="watchDialPanel">{hours}:{minutes}:{seconds}</h1>
+        </div>
+    )
+}
+
+export default Watch;
